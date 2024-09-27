d04c576550687aff6062fbb6b7303780eb832efa
Nestor Mosquera <yunsde18@gmail.com>
2024-09-26 19:54:15 -0500

    chore: information:

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -13,11 +13,11 @@ const Home: React.FC = async() => {
       <div className={styles.bg} >
         <div>
 
-  <video className={styles.video} poster="https://global.bowflex.com/on/demandware.static/-/Sites-nautilus-international-Library/default/dw1e7c91e7/bfx/homepage/posters/bowflex-hp-rebrand-launch-1920x800-poster-d.jpg" autoPlay={true} loop={true} playsInline tabIndex={0}>
+  <video className={styles.video} poster="https://global.bowflex.com/on/demandware.static/-/Sites-nautilus-international-Library/default/dw1e7c91e7/bfx/homepage/posters/bowflex-hp-rebrand-launch-1920x800-poster-d.jpg" autoPlay={true} loop={true} muted playsInline tabIndex={0}>
 <source src="https://download.bowflex.com/video/bfx/june23/bowflex-hp-rebrand-launch-1920x800.mp4" type="video/mp4" />
       </video>
       
-      <video className={styles.video_mobile} poster="https://global.bowflex.com/on/demandware.static/-/Sites-nautilus-international-Library/default/dwadc9d996/bfx/homepage/posters/bowflex-hp-rebrand-launch-428x613-poster-m.jpg" autoPlay={true} loop={true} playsInline tabIndex={0} >
+      <video className={styles.video_mobile} poster="https://global.bowflex.com/on/demandware.static/-/Sites-nautilus-international-Library/default/dwadc9d996/bfx/homepage/posters/bowflex-hp-rebrand-launch-428x613-poster-m.jpg" autoPlay={true} loop={true} muted playsInline tabIndex={0} >
 <source src="https://download.bowflex.com/video/bfx/june23/bowflex-hp-rebrand-launch-428x613.mp4" type="video/mp4" />
 </video>
         </div>
