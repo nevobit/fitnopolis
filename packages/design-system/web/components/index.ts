a3bf5338af4acbff98f0ca4c0d5630f431781138
Nestor Mosquera <yunsde18@gmail.com>
2024-10-07 09:48:35 -0500

    chore: nevogiate

--- packages/design-system/web/components/index.ts
+++ packages/design-system/web/components/index.ts
@@ -3,7 +3,6 @@ export { default as Field } from './Field';
 export { default as Input } from './Input';
 export * from './Loaders/Spinner';
 export * from './Loaders';
-export { default as Menus } from './Menus';
 export * from './Modal';
 export * from './Table';
 export { default as Textarea } from './Textarea';
